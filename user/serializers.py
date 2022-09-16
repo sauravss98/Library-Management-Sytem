9213ddaec67a6aeeadd6696d836aba1292b828d1
Inferno0990 <sauravsuresh171@gmail.com>
2022-09-16 16:44:34 +0530

    Small changes

--- user/serializers.py
+++ user/serializers.py
@@ -13,9 +13,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields=('id','username','idNumber','password')
         extra_kwargs={'password':{'write_only':True}}
 
-    # def create(self, validated_data):
-    #     user=User.objects.create_user(**validated_data)
-    #     return user
+
     def create(self, validated_data):
         user = User(
             idNumber=validated_data['idNumber'],
